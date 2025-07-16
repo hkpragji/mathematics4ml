9a2dd5d4282595b3786d9f78030a9f2d92e2bd29
Hiten Pragji <hkpragji@gmail.com>
2025-07-16 11:06:47 +0100

    M4ML Linear Algebra Lab: Python script to identify special matrices

---
+++ python/identify_special_matrix.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+def isSingular(A) :
+    B = np.array(A, dtype=np.float_)
+    try:
+        fixRowZero(B)
+        fixRowOne(B)
+        fixRowTwo(B)
+        fixRowThree(B)
+    except MatrixIsSingular:
+        return True
+    return False
+
+class MatrixIsSingular(Exception): pass
+
+def fixRowZero(A) :
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[1]
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[2]
+    if A[0,0] == 0 :
+        A[0] = A[0] + A[3]
+    if A[0,0] == 0 :
+        raise MatrixIsSingular()
+    A[0] = A[0] / A[0,0]
+    return A
+
+def fixRowOne(A) :
+    A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        A[1] = A[1] + A[2]
+        A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        A[1] = A[1] + A[3]
+        A[1] = A[1] - A[1,0] * A[0]
+    if A[1,1] == 0 :
+        raise MatrixIsSingular()
+    A[1] = A[1] / A[1,1]
+    return A
+
+def fixRowTwo(A) :
+    A[2] = A[2] - A[2,0] * A[0]
+    A[2] = A[2] - A[2,1] * A[1]
+    if A[2,2] == 0 :
+        A[2] = A[2] + A[3]
+        A[2] = A[2] - A[2,0] * A[0]
+        A[2] = A[2] - A[2,1] * A[1]
+    if A[2,2] == 0 :
+        raise MatrixIsSingular()
+    A[2] = A[2] / A[2,2]
+    return A
+
+def fixRowThree(A) :
+    A[3] = A[3] - A[3,0] * A[0]
+    A[3] = A[3] - A[3,1] * A[1]
+    if A[3,3] == 0 :
+        raise MatrixIsSingular()
+    A[3] = A[3] / A[3,3]
+    return A
+
+A = np.array([
+        [0, 7, -5, 3],
+        [2, 8, 0, 4],
+        [3, 12, 0, 5],
+        [1, 3, 1, 3]
+    ], dtype=np.float64)
+fixRowZero(A)
+fixRowOne(A)
+fixRowTwo(A)
+fixRowThree(A)
+print(A)
